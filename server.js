0cd5acf54654aa75cc6fe00665c0e3ae762fb6d4
Leon Carnagie <leonfross@gmail.com>
2020-07-29 11:18:05 -0700

    Populate repo with basic developer files

---
+++ server.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+// const mongojs = require("mongojs");
+
+// const app = express();
+
+// const databaseUrl = "workoutDB";
+// const collections = ["lastWorkout"];
+
+// const db = mongojs(databaseUrl, collections);
+
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
+
+// app.listen(3000, () => {
+//     console.log("App running on port 3000!");
+//   });
+  
