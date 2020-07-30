2d93f2c2e8f3e4387cdd772081ab40d46e51c699
Leon Carnagie <leonfross@gmail.com>
2020-07-30 12:13:34 -0700

    Modified server js and added dependencies

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-// const express = require("express");
-// const mongojs = require("mongojs");
+const express = require("express");
+const mongojs = require("mongojs");
 
-// const app = express();
+const app = express();
 
-// const databaseUrl = "workoutDB";
-// const collections = ["lastWorkout"];
+const databaseUrl = "workoutDB";
+const collections = ["lastWorkout"];
 
-// const db = mongojs(databaseUrl, collections);
+const db = mongojs(databaseUrl, collections);
 
-// db.on("error", error => {
-//   console.log("Database Error:", error);
-// });
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
 
-// app.listen(3000, () => {
-//     console.log("App running on port 3000!");
-//   });
+app.get("/name", (req, res) => {
+    db.lastWorkout.find() ({name
+      
+    }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    });
+  });
+
+  // Set the app to listen on port 3000
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+  });
   
